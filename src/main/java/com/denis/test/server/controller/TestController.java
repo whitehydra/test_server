a811a0306f791a25507f500c619e027cfdb91949
Denis Fadeev <whitehydra@yandex.ru>
2019-02-13 12:09:12 +0300

    Добавление Jackson

--- src/main/java/com/denis/test/server/controller/TestController.java
+++ src/main/java/com/denis/test/server/controller/TestController.java
@@ -1,17 +1,27 @@
 package com.denis.test.server.controller;
 
-import org.springframework.stereotype.Controller;
+import com.denis.test.server.entity.FirstEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/controller")
+@RestController
+@RequestMapping("/controller")                //url path контроллера
 public class TestController {
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getTestStart(ModelMap model){
-        return "Hello";
+    public FirstEntity getTestStart(){
+
+        return createEmptyEntity();
+    }
+
+    private FirstEntity createEmptyEntity() {
+        FirstEntity firstEntity = new FirstEntity();
+        firstEntity.setId(1);
+        firstEntity.setTitle("First entity");
+        firstEntity.setInfo("Hello!");
+        return firstEntity;
     }
 }
